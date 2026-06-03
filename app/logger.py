33f134b0f9ef14db01dddd01bfd544615094f175
Deepanshu Semwal <188905633+DaniSam-STU@users.noreply.github.com>
2026-06-03 22:59:40 +0530

    Update logger.py

--- app/logger.py
+++ app/logger.py
@@ -1,6 +1,6 @@
-import datetime
+import logging
 
+logging.basicConfig(level=logging.INFO)
 
 def log_event(message):
-    with open("security.log", "a", encoding="utf-8") as f:
-        f.write(f"{datetime.datetime.now()} - {message}\n")
+    logging.info(message)
